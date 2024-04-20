d2a312825ee93d6873ebbe5ef7fb59cd7ea4f06b
Magaiver <magaivermagalhaes.mm@gmail.com>
2024-04-20 00:11:24 -0300

    feat: ensure product  quantity can be modified on cart

--- src/providers/cart.tsx
+++ src/providers/cart.tsx
@@ -13,6 +13,7 @@ interface ICartContext {
   cartBasePrice: number;
   cartTotalDiscount: number;
   addProductToCart: (product: CartProduct) => void;
+  decreaseProductQuantity: (productId: string) => void;
 }
 
 export const CartContext = createContext<ICartContext>({
@@ -21,6 +22,7 @@ export const CartContext = createContext<ICartContext>({
   cartBasePrice: 0,
   cartTotalDiscount: 0,
   addProductToCart: () => {},
+  decreaseProductQuantity: () => {},
 });
 
 const CartProvider = ({ children }: { children: ReactNode }) => {
@@ -49,11 +51,32 @@ const CartProvider = ({ children }: { children: ReactNode }) => {
     //Se não , Adicione o produto a lista
     setProducts((prev) => [...prev, product]);
   };
+
+  const decreaseProductQuantity = (productId: string) => {
+    // Se a quantidade for 1 , remova o produto do carrinho
+
+    // Se nao , diminua  quantidade em 1
+    setProducts((prev) =>
+      prev
+        .map((cartProduct) => {
+          if (cartProduct.id === productId) {
+            return {
+              ...cartProduct,
+              quantity: cartProduct.quantity - 1,
+            };
+          }
+          return cartProduct;
+        })
+        .filter((cartProduct) => cartProduct.quantity > 0),
+    );
+  };
+
   return (
     <CartContext.Provider
       value={{
         products,
         addProductToCart,
+        decreaseProductQuantity,
         cartTotalPrice: 0,
         cartBasePrice: 0,
         cartTotalDiscount: 0,
